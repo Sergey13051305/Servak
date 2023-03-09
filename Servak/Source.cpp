138af7cfdb09a500422bbb5edfcda96fae184f40
shele <shele@MR_FERNANDO>
2023-03-09 17:24:36 +0500

    Pic

--- Servak/Source.cpp
+++ Servak/Source.cpp
@@ -1,8 +1,8 @@
-https://market.yandex.ru/product--materinskaia-plata-asus-z11pr-d16/1779556552/spec?cpc=cLJ3HhaPkIHibGs7sFrlxcEifkv78KleevhcE9VOoCaXnz9fppi9ghux-7F3OjKJ0hPGmm7clVxM8vMWuvryOiQXz3UBI-ryKWBv_Ope5UIHK7oZitX21iBVFJZmhXrSUDbUZ3Gaoxyn-2_bShMY7Zjvm4ANNsO2oz2MG9kiVMtWYDEQE_bx10XJjU3Go-9DRsWzIp7bS4SfFU0FMDvvklnqs45ykB3ffRMA3myRLIc8shOI657yTdBVylVF8rLI&track=char&sku=1953025528&cpa=1&nid=26912770 // Ã‡ÚÂËÌÍ‡
-https://www.citilink.ru/product/processor-dlya-serverov-dell-xeon-gold-6254-3-1ggc-338-brvq-1472246/properties/ // œÓˆÂÒÒÓ
-https://www.onlinetrade.ru/basket.html?basket_hash=51725e3de3837b8f114d4c5ffebbc6a7 // ŒÔÂ‡ÚË‚Í‡
-https://www.dns-shop.ru/product/7c6d16480f1329f2/servernyj-bp-exegate-serverpro-1000rads/ // ¡œ
+Ôªøhttps://market.yandex.ru/product--materinskaia-plata-asus-z11pr-d16/1779556552/spec?cpc=cLJ3HhaPkIHibGs7sFrlxcEifkv78KleevhcE9VOoCaXnz9fppi9ghux-7F3OjKJ0hPGmm7clVxM8vMWuvryOiQXz3UBI-ryKWBv_Ope5UIHK7oZitX21iBVFJZmhXrSUDbUZ3Gaoxyn-2_bShMY7Zjvm4ANNsO2oz2MG9kiVMtWYDEQE_bx10XJjU3Go-9DRsWzIp7bS4SfFU0FMDvvklnqs45ykB3ffRMA3myRLIc8shOI657yTdBVylVF8rLI&track=char&sku=1953025528&cpa=1&nid=26912770 // –ú–∞—Ç–µ—Ä–∏–Ω–∫–∞
+https://www.citilink.ru/product/processor-dlya-serverov-dell-xeon-gold-6254-3-1ggc-338-brvq-1472246/properties/ // –ü—Ä–æ—Ü–µ—Å—Å–æ—Ä
+https://www.onlinetrade.ru/basket.html?basket_hash=51725e3de3837b8f114d4c5ffebbc6a7 // –û–ø–µ—Ä–∞—Ç–∏–≤–∫–∞
+https://www.dns-shop.ru/product/7c6d16480f1329f2/servernyj-bp-exegate-serverpro-1000rads/ // –ë–ü
  // HDD
-https://market.yandex.ru/product--korpus-servernyi-3u-procase-re306-d0h14-c-48-14-3-5-int-chernyi-bez-bloka-pitaniia-glubina-480mm-mb-ceb-12-x10-5/1736834029?cpa=1&cpc=rkYm2UrE1SJyDT6itxfUPg8jCRPSA1Q0kpRsEpjCmcCqJDti_UyoXDXcVVpQ3HsM_LFOq8dXa1JCrf11FZFCKDIaAOtPkTODGGW2FUALdlT-M7NdvThH9ESB75TmQEdhbXwXsDu8ZIqCHYCYSeGFklvJzU4MnqiPnXT5Br_ISGfVyO36ljFf9Zt_EUU3PNDnAV6rjv0bHkSXVbRV_sIoEa_n1E5-iWAU7GPAQjtJEJLlpLrY3clgXA%2C%2C&sku=101649934472&offerid=OJ4WfO_0CsldHcCx3NQ7wg // ÓÔÛÒ
+https://market.yandex.ru/product--korpus-servernyi-3u-procase-re306-d0h14-c-48-14-3-5-int-chernyi-bez-bloka-pitaniia-glubina-480mm-mb-ceb-12-x10-5/1736834029?cpa=1&cpc=rkYm2UrE1SJyDT6itxfUPg8jCRPSA1Q0kpRsEpjCmcCqJDti_UyoXDXcVVpQ3HsM_LFOq8dXa1JCrf11FZFCKDIaAOtPkTODGGW2FUALdlT-M7NdvThH9ESB75TmQEdhbXwXsDu8ZIqCHYCYSeGFklvJzU4MnqiPnXT5Br_ISGfVyO36ljFf9Zt_EUU3PNDnAV6rjv0bHkSXVbRV_sIoEa_n1E5-iWAU7GPAQjtJEJLlpLrY3clgXA%2C%2C&sku=101649934472&offerid=OJ4WfO_0CsldHcCx3NQ7wg //–ö–æ—Ä–ø—É—Å
 
-»“Œ√Œ: 1†206†322 
+–ò–¢–û–ì–û: 1¬†206¬†322 —Ä
